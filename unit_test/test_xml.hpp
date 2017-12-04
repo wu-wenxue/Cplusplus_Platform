5f413316ce677aec0868f343dd5a0289bd732e82
wu-wenxue <wwx232324@126.com>
2017-12-04 13:20:36 +0800

    add

--- unit_test/test_xml.hpp
+++ unit_test/test_xml.hpp
@@ -186,8 +186,131 @@ bool updateUser(const char* xmlPath,User* user)
 
     if(userName != NULL)
     {
-
+        if(user->password != userNode->Attribute("Password"))
+        {
+            userNode->SetAttribute("Password",usr.password.c_str()); // modify property
+        }
+        XMLElement* genderNode = userNode->FirstChildElement("Gender");
+        if(user->gender != atoi(genderNode->GetText()))
+        {
+            genderNode->SetText(int2string(user->gender).c_str());
+        }
+        XMLElement* mobileNode = userNode->FirstChildElement("Mobile");
+        if(user->mobile != mobileNode->GetText())
+        {
+            mobileNode->SetText(user->mobile.c_str());
+        }
+        XMLElement* emailNode = userNode->FirstChildElement("Email");
+        if(user->email != emailNode->GetText())
+        {
+            emailNode->SetText(user->email.c_str());
+        }
+        if(doc.SaveFile(xmlPath) == 0)
+            return true;
     }
+    return false;
+}
+
+void test_xml3()
+{
+    User user("lvlv","0000011111",0,"12345667788","wuwenxue@123.com");
+    if(updateUser("./user.xml",&user))
+    {
+        cout << "update successfully" << endl;
+    } else {
+        cout << "update failed " << endl;
+    }
+    return ;
+}
+
+
+//function:删除指定节点内容
+//param:xmlPath:xml文件路径；userName：用户名称
+//return：bool
+bool deleteUserByName(const char* xmlPath,const string& username)
+{
+    XMLDocument doc;
+    if(doc.LoadFile(xmlPath) != 0)
+    {
+        cout << "load xml file failed " << endl;
+        return false;
+    }
+    XMLElement* root = doc.RootElement();
+    // doc.DeleteNode(root);  // delete all xml node
+
+    XMLElement* userNode = queryUserNodeByName(root,"userName");
+    if(userNode != NULL)
+    {
+        userNode->DeleteAttribute("Password");  // delete attribute
+        XMLElement* emailNode = userNode->FirstChildElement("Email");
+        userNode->DeleteChild(emailNode);  // delete assign node
+        //userNode->DeleteChildren();   // delete all children node
+        if(doc.SaveFile(xmlPath) == 0)
+            return true;
+    }
+    return false;
+}
+
+void test_xml4()
+{
+    if(deleteUserByName("./user.xml","lvlv"))
+        cout << "delete successfully" << endl;
+    else
+        cout << "delete failed " << endl;
+
+    return 0;
+}
+
+//function:获取xml文件申明
+//param:xmlPath:xml文件路径；strDecl：xml申明
+//return：bool
+bool getXMLDeclaration(const char* xmlPath,string& strDecl)
+{
+    XMLDocument doc;
+    if(doc.LoadFile(xmlPath) != 0)
+    {
+        cout << "load xml file failed" << endl;
+        return false;
+    }
+    XMLNode* decl = doc.FirstChild();
+    if(NULL != decl)
+    {
+        XMLDeclaration* declaration = decl->ToDeclaration();
+        if(NULL != declaration)
+        {
+            strDecl = declaration->Value();
+            return true;
+        }
+    }
+    return false;
+}
+
+void test_xml5()
+{
+    string strDecl;
+    if(getXMLDeclaration("./user.xml",strDecl))
+    {
+        cout << "declaration: " << strDecl << endl;
+    }
+    return 0;
+}
+
+//function:将xml文件内容输出到标准输出
+//param:xmlPath:xml文件路径
+void print(const char* xmlPath)
+{
+    XMLDocument doc;
+    if(doc.LoadFile("./user.xml") != 0)
+    {
+        cout << "load xml file failed " << endl;
+        return;
+    }
+    doc.Print();
+
+    //xml文件内容输出至内存
+    XMLPrinter printer;
+    doc.Print(&printer);
+
 }
 
 
@@ -201,6 +324,8 @@ void test_xml()
     User user("wuwenxue","123456",2,"123456789","123@123.com");
 
     insertXMLNode("../Cplusplus_PlatForm/data/test2.xml",user);
+
+    test_xml3();
 }
 
 #endif // TEST_XML_HPP
